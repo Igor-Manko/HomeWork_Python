298fd93a370386b212c05a1d63ac743311680275
Igor-Manko <manko161igor@gmail.com>
2022-10-06 00:22:29 +0300

    telephone directory

---
+++ HomeWork_7/main.py
@@ -0,0 +1,30 @@
+
+keys = ['Last Name', 'Name', 'Number']
+
+
+def input_read(path):
+    f = open(path, 'r', encoding='UTF8')
+    temp = f.read().split('\n')
+    f.close()
+    lines = []
+    for elem in temp:
+        temp_dict = {}
+        person = elem.split(' ')
+        for i in range(len(person)):
+            temp_dict[keys[i]] = person[i]
+        lines.append(temp_dict)
+    return lines
+
+
+def write_data(data, path):
+    f = open(path, 'w', encoding='UTF8')
+    for line in data:
+        temp = ''
+        for element in line:
+            temp += str(line[element]) + ' '
+        f.writelines(temp[:-1] + '\n')
+    f.close()
+
+
+print(input_read('input.txt'))
+write_data(input_read('input.txt'), 'data.txt')
